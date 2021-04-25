967f48b1cfe86f29f5411549a3cdd12d17b05004
Donquixote Sabo <mixboksu@gmail.com>
2021-04-25 19:23:43 +0200

    feat: :sparkles: Add GlobalStyles
    Global style contains CSS reset and Font Family

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 //styled-components setup
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/styles/theme';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
 
 function Root() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <div>SIEMA COP TAM</div>
     </ThemeProvider>
   );
