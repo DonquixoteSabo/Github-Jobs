0be2b04c8ecf2eb3d4855c11ea55e7c8764adfff
Donquixote Sabo <mixboksu@gmail.com>
2021-04-25 20:07:13 +0200

    feat: :sparkles: Add Router

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,12 +3,26 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/styles/theme';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
-
+//react-router-dom setup
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MainTemplate } from 'templates/MainTemplate';
+import { Job } from 'components/Job/Job';
 function Root() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>SIEMA COP TAM</div>
+      <Router>
+        <MainTemplate>
+          <Switch>
+            <Route path="/job">
+              <Job />
+            </Route>
+            <Route path="/">
+              <h1>siema</h1>
+            </Route>
+          </Switch>
+        </MainTemplate>
+      </Router>
     </ThemeProvider>
   );
 }
