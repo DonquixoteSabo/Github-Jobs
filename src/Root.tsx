8bef0fd0c176ee0c99fb2f98a6eeb726a54cdc6d
Donquixote Sabo <mixboksu@gmail.com>
2021-04-25 10:58:18 +0200

    build: :tada: Begin a project

---
+++ src/Root.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Root() {
+  return (
+    <div>
+      SIEMA{' '}
+      <strong>
+        co tam <h1>u ciebie</h1>
+      </strong>
+    </div>
+  );
+}
+
+export { Root };
