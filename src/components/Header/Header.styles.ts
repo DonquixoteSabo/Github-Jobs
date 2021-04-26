b761c79c99ce6d5739cec151ffe588ef197cc708
Donquixote Sabo <mixboksu@gmail.com>
2021-04-26 18:33:27 +0200

    refactor: :recycle: Move styles that affect to position at the very top of component

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Logo = styled.h1`
-  color: ${({ theme }) => theme.colors.steelGray};
   font-size: ${({ theme }) => theme.fontSize.xl};
+  color: ${({ theme }) => theme.colors.steelGray};
   font-weight: 300;
   strong {
     font-weight: 700;
