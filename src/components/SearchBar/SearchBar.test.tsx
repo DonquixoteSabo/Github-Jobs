1a6b706836afaea9142bb92e90f05ee0b0d8daea
Donquixote Sabo <mixboksu@gmail.com>
2021-04-27 06:41:39 +0200

    feat: :sparkles: Add SearchBar styles

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -7,13 +7,15 @@ import { SearchBar } from './SearchBar';
 describe('SearchBar', () => {
   it('renders SearchBar', () => {
     renderWithProviders(<SearchBar />);
-    expect(screen.getByPlaceholderText('Title, companies')).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText('Title, companies, expertise or benefits')
+    ).toBeInTheDocument();
     expect(screen.getByText('Search')).toBeInTheDocument();
   });
   it('clears input after submitting a form', () => {
     renderWithProviders(<SearchBar />);
     const input = screen.getByPlaceholderText(
-      'Title, companies'
+      'Title, companies, expertise or benefits'
     )! as HTMLInputElement;
     const form = screen.getByRole('search')! as HTMLFormElement;
     fireEvent.change(input, { target: { value: 'Hello' } });
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,55 @@
 import React, { useState } from 'react';
+import { BsBag } from 'react-icons/bs';
 
 import styled from 'styled-components';
 import backgroundImg from 'assets/img/backgroundImg.png';
 export const Wrapper = styled.section`
   width: 100%;
   height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 40px 0;
   background-image: url(${backgroundImg});
   background-size: cover;
+  border-radius: 8px;
+  form {
+    width: 90%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #fff;
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+    div {
+      display: flex;
+      align-items: center;
+      margin-left: 20px;
+      width: 100%;
+    }
+  }
 `;
-export const Input = styled.input``;
-export const Button = styled.button``;
+
+export const Input = styled.input`
+  padding: 15px 20px;
+  border: none;
+  outline: none;
+  width: 100%;
+  &::placeholder {
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.fontSize.xs};
+    color: ${({ theme }) => theme.colors.heather};
+  }
+`;
+export const Button = styled.button`
+  padding: 12px 25px;
+  margin-right: 5px;
+  font-weight: 500;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.blue};
+  color: #fff;
+  cursor: pointer;
+  border-radius: 4px;
+`;
 
 const SearchBar = () => {
   const [inputValue, setInputValue] = useState('');
@@ -21,14 +61,17 @@ const SearchBar = () => {
   return (
     <Wrapper>
       <form className="input-wrapper" onSubmit={handleSubmit} role="search">
-        <Input
-          type="text"
-          placeholder="Title, companies"
-          value={inputValue}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setInputValue(event.target.value)
-          }
-        />
+        <div>
+          <BsBag />
+          <Input
+            type="text"
+            placeholder="Title, companies, expertise or benefits"
+            value={inputValue}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setInputValue(event.target.value)
+            }
+          />
+        </div>
         <Button type="submit">Search</Button>
       </form>
     </Wrapper>
