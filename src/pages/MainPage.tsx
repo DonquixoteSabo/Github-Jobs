8fa7fbdb4e3c5604661482b3e0c636c328586fdf
Donquixote Sabo <mixboksu@gmail.com>
2021-04-27 19:09:46 +0200

    chore: :poop: Test how to fetch this API and fix CORS policy
    I will delete this code later. This was just a test I wanted to be sure that I will made a mistake in thunk middleware,. not in just basic API fetching

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 
+const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/';
+
 function MainPage() {
+  useEffect(() => {
+    (async () => {
+      const result = await axios.get(
+        `${CORS_ANYWHERE}https://jobs.github.com/positions.json?&page=1`
+      );
+      const { data: jobs } = result;
+      console.log(jobs);
+    })();
+  }, []);
   return (
     <>
       <SearchBar />
