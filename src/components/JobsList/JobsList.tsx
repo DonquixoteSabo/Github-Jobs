c38b2800ea0cfd76e5059e88555f87edf4e71420
Donquixote Sabo <mixboksu@gmail.com>
2021-04-28 07:39:47 +0200

    feat: :sparkles: Get jobs from globalState and map over them

--- src/components/JobsList/JobsList.tsx
+++ src/components/JobsList/JobsList.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 //components
 import { JobCard } from 'components/JobCard/JobCard';
+//hooks
+import { useAppSelector } from 'hooks/reduxHook';
 
 function JobsList() {
+  const { jobs } = useAppSelector((state) => state.jobs);
   return (
     <>
       <div>Our list</div>
-      {/* <JobCard /> */}
+      <ul>
+        {jobs.length > 0 ? (
+          jobs.map((job) => <JobCard {...job} />)
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </ul>
     </>
   );
 }
