9190998e45ac153d50b4d21f6febed40299679bb
Donquixote Sabo <mixboksu@gmail.com>
2021-04-28 06:27:44 +0200

    perf: :zap: Clear useEffect in MainPage
    Clear it in order not to block cors-anywhere API

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,19 +1,7 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 
-const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/';
-
 function MainPage() {
-  useEffect(() => {
-    (async () => {
-      const result = await axios.get(
-        `${CORS_ANYWHERE}https://jobs.github.com/positions.json?&page=1`
-      );
-      const { data: jobs } = result;
-      console.log(jobs);
-    })();
-  }, []);
   return (
     <>
       <SearchBar />
