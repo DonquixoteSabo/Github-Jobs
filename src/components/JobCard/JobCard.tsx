3fa554fece5ab3b3f742b0918185c425a3e1b252
Donquixote Sabo <mixboksu@gmail.com>
2021-04-28 07:09:57 +0200

    feat: :sparkles: Create JobList and JobCard with styles

---
+++ src/components/JobCard/JobCard.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { BiWorld } from 'react-icons/bi';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+
+//styles
+import styled from 'styled-components';
+export const CardWrapper = styled.div`
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+  display: grid;
+  grid-template-columns: 90px auto 1fr;
+  grid-template-rows: auto 1fr;
+  grid-gap: 15px;
+  padding: 12px;
+`;
+export const CardLogo = styled.img`
+  width: 100%;
+  height: auto;
+`;
+export const CardBody = styled.div`
+  color: ${({ theme }) => theme.colors.chambray};
+  .company {
+    font-weight: 600;
+    font-size: ${({ theme }) => theme.fontSize.s};
+  }
+  .title {
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+  .type {
+    width: 75px;
+    height: 26px;
+    border: 1px solid ${({ theme }) => theme.colors.chambray};
+    border-radius: 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+export const CardInfo = styled.div`
+  grid-column: 2/3;
+  font-weight: 500;
+  font-size: ${({ theme }) => theme.fontSize.s};
+  color: #b9bdcf;
+  display: flex;
+  justify-content: space-between;
+  div {
+    display: flex;
+  }
+`;
+function JobCard() {
+  return (
+    <CardWrapper>
+      <CardLogo src="https://picsum.photos/200" />
+      <CardBody>
+        <h3 className="company">Kasisto</h3>
+        <h1 className="title">Front-End Software Engineer</h1>
+        <div className="type">
+          <p className="company">Full time</p>
+        </div>
+      </CardBody>
+      <CardInfo>
+        <div>
+          <BiWorld />
+          <p className="city">New York</p>
+        </div>
+        <div>
+          <AiOutlineClockCircle />
+          <p className="time">5 days ago</p>
+        </div>
+      </CardInfo>
+    </CardWrapper>
+  );
+}
+
+export { JobCard };
--- src/components/JobsList/JobsList.tsx
+++ src/components/JobsList/JobsList.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+//components
+import { JobCard } from 'components/JobCard/JobCard';
 
 function JobsList() {
-  return <div>Our list</div>;
+  return (
+    <>
+      <div>Our list</div>
+      <JobCard />
+    </>
+  );
 }
 
 export { JobsList };
