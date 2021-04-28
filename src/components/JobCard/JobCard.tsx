edec12e9a8a172fe44fbcc6e4d7d77dd12a472d1
Donquixote Sabo <mixboksu@gmail.com>
2021-04-28 07:20:40 +0200

    refactor: :art: Move Styled Components to new file

--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -1,84 +1,8 @@
 import React from 'react';
 import { BiWorld } from 'react-icons/bi';
 import { AiOutlineClockCircle } from 'react-icons/ai';
-
 //styles
-import styled from 'styled-components';
-export const CardWrapper = styled.div`
-  width: 100%;
-  background-color: white;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-  display: grid;
-  grid-template-columns: 90px auto 1fr;
-  grid-template-rows: auto 1fr;
-  grid-gap: 15px;
-  padding: 12px;
-  cursor: pointer;
-  @media (min-width: ${({ theme }) => theme.breakpoints.m}) {
-    grid-template-columns: 90px 1fr auto;
-  }
-`;
-export const CardLogo = styled.img`
-  width: 100%;
-  height: auto;
-  border-radius: 4px;
-`;
-export const CardBody = styled.div`
-  color: ${({ theme }) => theme.colors.chambray};
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  .company {
-    font-weight: 600;
-    font-size: ${({ theme }) => theme.fontSize.s};
-    @media (min-width: ${({ theme }) => theme.breakpoints.m}) {
-      font-size: ${({ theme }) => theme.fontSize.m};
-    }
-  }
-  .title {
-    font-weight: 400;
-    font-size: ${({ theme }) => theme.fontSize.xl};
-    @media (min-width: ${({ theme }) => theme.breakpoints.m}) {
-      font-size: ${({ theme }) => theme.fontSize.xxl};
-    }
-  }
-  .type {
-    width: 75px;
-    height: 26px;
-    border: 1px solid ${({ theme }) => theme.colors.chambray};
-    border-radius: 4px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    @media (min-width: ${({ theme }) => theme.breakpoints.m}) {
-      width: 85px;
-      height: 30px;
-    }
-  }
-`;
-export const CardInfo = styled.div`
-  grid-column: 2/3;
-  font-weight: 500;
-  font-size: ${({ theme }) => theme.fontSize.s};
-  color: #b9bdcf;
-  display: flex;
-  justify-content: space-between;
-  div {
-    display: flex;
-    p {
-      margin-left: 10px;
-    }
-  }
-  @media (min-width: ${({ theme }) => theme.breakpoints.m}) {
-    grid-column: 3/4;
-    margin-top: auto;
-    font-size: ${({ theme }) => theme.fontSize.m};
-    div {
-      margin-left: 30px;
-    }
-  }
-`;
+import { CardWrapper, CardLogo, CardBody, CardInfo } from './JobCard.styles';
 function JobCard() {
   return (
     <CardWrapper>
