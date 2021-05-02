b975aac159db48bab6cbdc577bc53351844059a2
Donquixote Sabo <mixboksu@gmail.com>
2021-05-02 07:04:47 +0200

    docs: :bulb: Add Comment in fetchAllJobs

--- src/API/fetchAllJobs.ts
+++ src/API/fetchAllJobs.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchAllJobs = createAsyncThunk('jobs/fetchAllJobs', async () => {
+  //Right now im too tired to work with it. I will think about it few days laters. Right now I dont really care what will I do before deployment and how this endpoint will word.
   const response = await axios.get('/jobs');
   const data = await response.data;
   return data;
