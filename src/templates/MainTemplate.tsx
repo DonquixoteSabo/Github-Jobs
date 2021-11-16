14bb3a5757326a848b2730c2b250f1e8e107f1a3
DonquixoteSabo <mixboksu@gmail.com>
2021-11-16 06:25:19 +0100

    feat: Add footer

--- src/templates/MainTemplate.tsx
+++ src/templates/MainTemplate.tsx
@@ -2,11 +2,22 @@ import React, { ReactNode } from 'react';
 //components
 import { Header } from 'components/Header/Header';
 //styles
-import { Wrapper } from './MainTemplate.styles';
+import { Footer, Wrapper } from './MainTemplate.styles';
 
 export const MainTemplate = ({ children }: { children: ReactNode }) => (
   <Wrapper>
     <Header />
     {children}
+    <Footer>
+      created by
+      <a
+        href="https://github.com/DonquixoteSabo"
+        target="_blank"
+        rel="noreferrer"
+      >
+        DonquixoteSabo
+      </a>
+      - devChallenges.io
+    </Footer>
   </Wrapper>
 );
